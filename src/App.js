27c8aeb4a565e370831a61ef4d06f59251486812
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-02 11:18:35 +0300

    Refactor code file structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Books from './components/BooksContainer';
-import BookCategories from './components/BookCategories';
+import Navigation from './components/UI/Navigation';
+import Books from './components/redux/books/Book';
+import BookCategories from './components/redux/categories/BookCategories';
 
 function App() {
   return (
     <div>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Books />} />
+        <Route path="/" element={<Books title="Ben" author="Ten" />} />
         <Route path="/categories" element={<BookCategories />} />
       </Routes>
     </div>
