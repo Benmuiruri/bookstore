f94940c25fbf48ae8f06082a37b44ea63028ad70
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-02 12:40:27 +0300

    make categories action semantically clear

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,9 +1,9 @@
-const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
+const VIEW_CATEGORIES = 'bookstore/categories/CHECK_STATUS';
 const initialState = [];
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case CHECK_STATUS:
+    case VIEW_CATEGORIES:
       return 'Page under construction';
     default:
       return state;
@@ -11,5 +11,5 @@ export default function reducer(state = initialState, action) {
 }
 
 export const checkStatus = () => ({
-  type: CHECK_STATUS,
+  type: VIEW_CATEGORIES,
 });
