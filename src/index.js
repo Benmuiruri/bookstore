4a74856c380d2c537cb366a6a444f00059cfc7ae
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-03 15:56:11 +0300

    Add provider and store in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/configureStore';
 
 import './index.css';
 import App from './App';
@@ -10,7 +12,9 @@ const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement,
