9aa5b6e32611876c4599445a3afc68c5844e5889
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-05 16:45:34 +0300

    Style nav header

--- src/components/UI/Navigation.js
+++ src/components/UI/Navigation.js
@@ -3,12 +3,15 @@ import Header from './Header';
 import styles from './Navigation.module.css';
 
 function Navigation() {
-  const links = [
+  const mainLinks = [
     {
       id: 1,
       path: '/',
       text: 'Books',
     },
+  ];
+
+  const subLinks = [
     {
       id: 2,
       path: '/categories',
@@ -20,9 +23,24 @@ function Navigation() {
     <nav className={styles.headerNav}>
       <Header />
       <ul className={styles.navigation}>
-        {links.map((link) => (
+        {mainLinks.map((link) => (
           <li key={link.id} className={styles.navLink}>
-            <NavLink data-testid={link.id} to={link.path} className={styles.link}>
+            <NavLink
+              data-testid={link.id}
+              to={link.path}
+              className={styles.link}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+        {subLinks.map((link) => (
+          <li key={link.id} className={styles.navLink}>
+            <NavLink
+              data-testid={link.id}
+              to={link.path}
+              className={styles.offLink}
+            >
               {link.text}
             </NavLink>
           </li>
